7e9e6c26a78369c7f812e1720e94b339be495e33
Nidhi <nidhiparabmail@gmail.com>
2023-05-06 07:50:11 +0530

    home

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -9,50 +9,15 @@ const Header = () => {
 	return (
 		<Container className="header " id="header">
 			<Row className="content">
-            <Col>
-            <h1>Software <br/> Developer.</h1>
-            <p>Front End Addict.</p>
-            </Col>
-            <Col className="name align-self-end">
+            <Col >
             <h1>Nidhi <br/> Parab.</h1>
             <p>The Art & The Artist.</p>
             </Col>
+            <Col className="name align-self-end">
+            <h1>Software <br/> Developer.</h1>
+            <p>Front End Addict.</p>
+            </Col>
             
-                {/* <a href="#about"><h1 className="">About Me</h1> </a>
-                <a href="#edu"><h1 className="">Education</h1> </a>
-                        <a href="#skill"><h1 className="skill">Skills</h1> </a>
-                        <a href="#experience"><h1 >Experience</h1> </a>
-                        <a href="#projects"><h1 className="proj">Projects</h1> </a>
-                        <a href="#certi"><h1 className="certi">Certifications</h1> </a>
-                        <a href="#positions"><h1 className="positions">Positions</h1> </a> */}
-
-            
-                {/* <ul type="none">
-                    <li className="">
-                        <a href="#about"><h1 className="about">About Me</h1> </a>
-                    </li>
-                    <li className="">
-                        <a href="#edu"><h1 className="edu">Education</h1> </a>
-                    </li>
-                    <li className="d-none d-sm-block list">
-                        <a href="#skill"><h1 className="skill">Skills</h1> </a>
-                    </li>
-                    <li className="d-none d-sm-block list">
-                        <a href="#experience"><h1 >Experience</h1> </a>
-                    </li>
-                    <li className="">
-                        <a href="#projects"><h1 className="proj">Projects</h1> </a>
-                    </li>
-                    <li className="d-none d-sm-block">
-                        <a href="#certi"><h1 className="certi">Certifications</h1> </a>
-                    </li>
-                    <li className="d-none d-sm-block">
-                        <a href="#positions"><h1 className="positions">Positions</h1> </a>
-                    </li>
-                    <li className="">
-                        <a href="mailto:nidhiparabmail@gmail.com"><h1 className="contact">Get in Touch</h1> </a>
-                    </li>
-                </ul> */}
 			</Row>
 		</Container>
 	);
